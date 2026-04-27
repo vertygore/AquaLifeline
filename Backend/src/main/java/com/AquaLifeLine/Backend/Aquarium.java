d9cf002bcdd16a059fe66e8ca8d3a126280e239c
Angelo <walburger.privat@gmail.com>
2026-04-27 10:24:49 +0200

    mvnwrapper

--- Backend/src/main/java/com/AquaLifeLine/Backend/Aquarium.java
+++ Backend/src/main/java/com/AquaLifeLine/Backend/Aquarium.java
@@ -1,11 +1,16 @@
 package com.AquaLifeLine.Backend;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
